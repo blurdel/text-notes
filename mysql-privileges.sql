7e747fa2dea9f0007292ae052acafa5069b094a4
blurdel <5648737+blurdel@users.noreply.github.com>
2025-12-17 15:34:35 -0500

    added mysql

---
+++ mysql-privileges.sql
@@ -0,0 +1,19 @@
+DROP DATABASE IF EXISTS bookdb;
+DROP USER IF EXISTS `bookadmin`@`%`;
+DROP USER IF EXISTS `bookuser`@`%`;
+
+-- database
+CREATE DATABASE IF NOT EXISTS bookdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- admin
+CREATE USER IF NOT EXISTS `bookadmin`@`%` IDENTIFIED BY 'password';
+-- privileges
+GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW,
+CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `bookdb`.* TO `bookadmin`@`%`;
+
+-- user
+CREATE USER IF NOT EXISTS `bookuser`@`%` IDENTIFIED BY 'password';
+-- privileges
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `bookdb`.* TO `bookuser`@`%`;
+
+FLUSH PRIVILEGES;
